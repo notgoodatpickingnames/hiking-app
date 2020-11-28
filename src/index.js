0ceb63129cbe0826f5c6a03aa2426f067e8041bd
notgoodatpickingnames <32577550+notgoodatpickingnames@users.noreply.github.com>
2020-11-27 20:09:36 -0600

    Integrated MapBox

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFjb2JiMTk5NiIsImEiOiJja2kwbXo4eWwwd3NxMnhwZGY2MDMzZ3gzIn0.liz-_iujMMUmnOG2Cys6og'; // TODO - This is public.. get your own token.
+
+let store = createStore(
+        reducers,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // This should be removed before code is submitted.
+    );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
