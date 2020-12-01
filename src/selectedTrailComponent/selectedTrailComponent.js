dbf90a4433bd82a85da0dd55de5a895d6100dbfd
notgoodatpickingnames <32577550+notgoodatpickingnames@users.noreply.github.com>
2020-11-30 18:39:01 -0600

    Finished Selected Trail Area

--- src/selectedTrailComponent/selectedTrailComponent.js
+++ src/selectedTrailComponent/selectedTrailComponent.js
@@ -1,3 +1,5 @@
+import './selectedTrailComponent.css';
+
 import React from 'react';
 import { connect } from "react-redux";
 
@@ -10,6 +12,34 @@ export class SelectedTrailComponent extends React.Component {
                 </div>
                 
                 <div>
+                    {this.props.trailSelected.location}
+                </div>
+
+                <div className="data">
+                    <div>
+                        {this.props.trailSelected.length} Miles
+                    </div>
+
+                    <div className="row">
+                        <div>
+                            {this.props.trailSelected.ascent}' Up
+                        </div>
+
+                        <div>
+                            {this.props.trailSelected.descent}' Down
+                        </div>
+
+                        <div>
+                            {this.props.trailSelected.high}' High
+                        </div>
+
+                        <div>
+                            {this.props.trailSelected.low}' Low
+                        </div>
+                    </div>
+                </div>
+
+                <div className="summary">
                     {this.props.trailSelected.summary}
                 </div>
             </div>
